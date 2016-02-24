9c60ec52665e29b227d7d96567e4b79160108016
fouric <fouric@protonmail.com>
2016-02-24 13:49:28 -0800

    Actually works now...

--- lispbot.lisp
+++ lispbot.lisp
@@ -3,14 +3,24 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (ql:quickload :cl-irc))
 
+(defparameter *connection* nil)
+
+(defun part ()
+  (when *connection*
+    (irc:quit *connection*)
+    (setf *connection* nil)))
+
 (defun test ()
   (flet ((message-received-hook (message)
-	   (format t "HALP~%")
-	   ;(format t "message received: ~A~%" message)
-	   ))
-    (let ((connection (irc:connect :nickname "lispbot" :server "irc.cat.pdx.edu")))
-      (irc:join connection "#bots")
-      (irc:add-hook connection 'irc-message-event #'message-received-hook)
-      (irc:add-hook connection 'dcc-message-event #'message-received-hook)
-      (irc:add-hook connection 'default-event #'message-received-hook)
-      (irc:read-message-loop connection))))
+	   (let* ((arguments (irc:arguments message))
+		  (channel (first arguments))
+		  (contents (second arguments))
+		  (sender (irc:source message)))
+	     (format t "message received: ~A~%" (irc:arguments message))
+	     (irc:privmsg *connection* sender (format nil "you said: ~A" contents))
+	     )))
+    (unless *connection*
+      (setf *connection* (irc:connect :nickname "lispbot" :server "irc.cat.pdx.edu")))
+    (irc:join *connection* "#bots")
+    (irc:add-hook *connection* 'irc:irc-privmsg-message #'message-received-hook)
+    (irc:read-message-loop *connection*)))
