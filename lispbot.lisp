c7e6b585c5a6a0f52d48b01602d28d51c12a1c30
fouric <fouric@protonmail.com>
2016-05-19 12:22:19 -0700

    Made code slightly more beginner-friendly

--- lispbot.lisp
+++ lispbot.lisp
@@ -1,12 +1,14 @@
 (eval-when (:compile-toplevel :load-toplevel :execute) (declaim (optimize (debug 3))))
 
-(eval-when (:compile-toplevel :load-toplevel :execute) (ql:quickload :cl-irc))
-(eval-when (:compile-toplevel :load-toplevel :execute) (ql:quickload :cl+ssl))
-(eval-when (:compile-toplevel :load-toplevel :execute) (ql:quickload :split-sequence))
-(eval-when (:compile-toplevel :load-toplevel :execute) (ql:quickload :trivial-shell))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (progn
+    (ql:quickload :cl-irc)
+    (ql:quickload :cl+ssl)
+    (ql:quickload :split-sequence)
+    (ql:quickload :trivial-shell)))
 
 (defpackage :lispbot (:use :common-lisp :irc :cl+ssl :split-sequence)
-	    (:export :run :part))
+	    (:export #:run #:part))
 (in-package :lispbot)
 
 (defparameter *connection* nil)
@@ -33,8 +35,8 @@
 (defun run ()
   (flet ((message-received-hook (message)
 	   (let* ((arguments (irc:arguments message))
-		  ;;(channel (first arguments))
 		  (contents (second arguments))
+		  ;;(channel (first arguments))
 		  ;;(sender (irc:source message))
 		  (split (split-sequence:split-sequence #\Space contents))
 		  (head (first split))
@@ -47,8 +49,13 @@
 	       ("source"
 		(irc:privmsg *connection* "#bots" "https://github.com/fouric/lispbot"))))))
     (unless *connection*
-      (setf *connection* (irc:connect :nickname "lispbot" :server "irc.cat.pdx.edu" :port 6697 :connection-security :ssl)))
-    (irc:join *connection* "#bots" :password (getf (with-open-file (in "auth.dat") (with-standard-io-syntax (read in))) :key))
-    ;;(irc:join *connection* "#robots" :password (getf (with-open-file (in "auth.dat") (with-standard-io-syntax (read in))) :key))
+      (setf *connection* (irc:connect :nickname "lispbot"
+				      :server "irc.cat.pdx.edu"
+				      :port 6697
+				      :connection-security :ssl)))
+    (let ((auth-file (open "auth.dat")))
+      (let ((auth-data (read auth-file)))
+	(irc:join *connection* "#bots" :password (getf auth-data :key)))
+      (close auth-file))
     (irc:add-hook *connection* 'irc:irc-privmsg-message #'message-received-hook)
     (irc:read-message-loop *connection*)))
