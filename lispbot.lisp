438831cf8c99931f0459fe9af1003ef1acd71735
fouric <fouric@protonmail.com>
2016-05-19 19:56:43 -0700

    FOOBAR

--- lispbot.lisp
+++ lispbot.lisp
@@ -1,7 +1,6 @@
-(eval-when (:compile-toplevel :load-toplevel :execute) (declaim (optimize (debug 3))))
-
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (progn
+    (declaim (optimize (debug 3)))
     (ql:quickload :cl-irc)
     (ql:quickload :cl+ssl)
     (ql:quickload :split-sequence)
@@ -32,30 +31,39 @@
     (irc:quit *connection*)
     (setf *connection* nil)))
 
+;; a "hook" is some sort of function that we run in response to an event
+;; this one happens to take one parameter that is filled in with a message by cl-irc
+(defun message-received-hook (message)
+  (let* ((arguments (irc:arguments message))
+	 (contents (second arguments))
+	 ;;(channel (first arguments))
+	 (sender (irc:source message))
+	 (split (split-sequence:split-sequence #\Space contents))
+	 (head (first split))
+	 (body (rest split)))
+    (format t "The sender of the message is: ~a~%" sender)
+    (when (search "dbot" head))
+    (fn-case (first body) #'string=
+	     ("drop"
+	      (part))
+	     ("source"
+	      (irc:privmsg *connection* "#bots" "https://github.com/fouric/lispbot"))
+	     ;; this line will cause the bot to reply to whoever said "hello" to it
+	     ("hello"
+	      (let ((string-to-send (format nil "Hello, ~A!" sender)))
+		;; make the string lowercase so that we don't have to use lowercase ourselves
+		(let ((lowercase-string (string-downcase string-to-send)))
+		  (irc:privmsg *connection* "#bots" string-to-send)))))))
+
 (defun run ()
-  (flet ((message-received-hook (message)
-	   (let* ((arguments (irc:arguments message))
-		  (contents (second arguments))
-		  ;;(channel (first arguments))
-		  ;;(sender (irc:source message))
-		  (split (split-sequence:split-sequence #\Space contents))
-		  (head (first split))
-		  (body (rest split)))
-	     (print (cons head body))
-	     (when (search "dbot" head))
-	     (fn-case (first body) #'string=
-	       ("drop"
-		(part))
-	       ("source"
-		(irc:privmsg *connection* "#bots" "https://github.com/fouric/lispbot"))))))
-    (unless *connection*
-      (setf *connection* (irc:connect :nickname "lispbot"
-				      :server "irc.cat.pdx.edu"
-				      :port 6697
-				      :connection-security :ssl)))
-    (let ((auth-file (open "auth.dat")))
-      (let ((auth-data (read auth-file)))
-	(irc:join *connection* "#bots" :password (getf auth-data :key)))
-      (close auth-file))
-    (irc:add-hook *connection* 'irc:irc-privmsg-message #'message-received-hook)
-    (irc:read-message-loop *connection*)))
+  (unless *connection*
+    (setf *connection* (irc:connect :nickname "lispbot"
+				    :server "irc.cat.pdx.edu"
+				    :port 6697
+				    :connection-security :ssl)))
+  (let ((auth-file (open "auth.dat")))
+    (let ((auth-data (read auth-file)))
+      (irc:join *connection* "#bots" :password (getf auth-data :key)))
+    (close auth-file))
+  (irc:add-hook *connection* 'irc:irc-privmsg-message #'message-received-hook)
+  (irc:read-message-loop *connection*))
